d7ba3b77921b9924a3ffbc2953a8bd1c1d876534
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-10-27 14:50:56 +0300

    Login Component

--- ecommerce/src/app/modules/auth-profile/login/login.component.ts
+++ ecommerce/src/app/modules/auth-profile/login/login.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  email:any = null;
+  password:any = null;
+
+
+  constructor(
+    public authService:AuthService,
+    public router:Router
+  ){}
+
+  ngOnInit(): void{
+    if(this.authService.user && this.authService.token){
+      this.router.navigate(["/"])
+    }
+  }
+
+
+  login(){
+
+    if(!this.email || !this.password){
+      alert("Error Username and password");
+      return;
+    }
+    this.authService.login(this.email, this.password).subscribe((resp:any)=>{
+
+      if(!resp.error && resp){
+        console.log(resp);
+      }
+      else{
+        if(resp.error.error == 'Unauthorized'){
+          alert("No Entry");
+          return;
+        }
+      }
+
+
+    })
+
+
+  }
 
 }
