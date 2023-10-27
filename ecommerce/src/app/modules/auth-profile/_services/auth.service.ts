7b5109fd0fc40c88dfa24880cdd49ad2e8a79786
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-10-27 11:00:58 +0300

    Auth Service
    Youtube Efe Görkem Ümit

--- ecommerce/src/app/modules/auth-profile/_services/auth.service.ts
+++ ecommerce/src/app/modules/auth-profile/_services/auth.service.ts
@@ -1,9 +1,87 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs';
+import { URL_SERVICE } from 'src/config/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+
+
+  ) { 
+
+    this.loadStorage()
+  }
+
+  loadStorage(){
+    if(this.loadStorage.getItem("token"))
+    {
+      this.token= localStorage.getItem("token");
+      this.user = JSON.parse(localStorage.getItem("user") ?? '');
+    }
+    else{
+      this.token = '';
+      this.user = null;
+    }
+
+
+  }
+
+  login(email : string, password : string){
+    let URL = URL_SERVICE + '/login';
+    return this.http.post(URL, {email,password}).pipe(
+      map((resp:any)=>{
+        if(resp.access_token){
+           return.this.saveLocalStorageResponse(resp);
+        }
+        else{
+          return resp;
+        }
+
+
+      }),
+      catchError((err:any)=>{
+        return of(err);
+      })
+    )
+  }
+
+  saveLocalStorageResponse(resp.any){
+    if(resp.access_token && resp.user){
+      this.loadStorage.setItem("token", resp.access_token);
+      this.loadStorage.setItem("user", JSON.stringify(resp.user));
+      this.user = resp.user;
+      this.token = resp.access_token
+      return true;
+
+    }
+    return false;
+
+  }
+
+
+  register(data:any){
+    let URL = URL_SERVICE+ '/register';
+    return this.http.post(URL,data);
+  }
+
+
+
+  logout(){
+    this.user=null;
+    this.token = '';
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.router.navigate(["auth/login"])
+
+  }
+
+
+
 }
