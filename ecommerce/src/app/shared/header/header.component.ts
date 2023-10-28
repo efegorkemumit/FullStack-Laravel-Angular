ca6dde3b01093c5201149b5615ef40a370016e89
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-10-28 13:20:44 +0300

    Login Visible
    Youtube Efe Görkem Ümit

--- ecommerce/src/app/shared/header/header.component.ts
+++ ecommerce/src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/modules/auth-profile/_services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  constructor(
+    public authService:AuthService,
+    public router:Router
+  ){}
 
   mobileMenuVisible: boolean =false;
 
--- ecommerce/src/app/shared/shared.module.ts
+++ ecommerce/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { AuthProfileRoutingModule } from '../modules/auth-profile/auth-profile-routing.module';
 
 
 
@@ -13,7 +14,8 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    AuthProfileRoutingModule
   ],
   exports:[
     HeaderComponent,
