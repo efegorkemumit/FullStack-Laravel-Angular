af920ee95527c2a11133c96817b4b00b2a91b35b
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-10-29 19:26:45 +0300

    Admin Controller
    Youtube Efe Görkem Ümit

---
+++ api-ecommerce/app/Http/Controllers/Api/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+   
+    public function getLastUsers()
+    {
+        $userdata = auth()->user();
+
+        if($userdata->is_admin == "1"){
+           
+            $latestUsers = User::latest()->take(5)->get();
+            return response()->json(['latest_user'=>$latestUsers]);
+        }
+
+
+        
+    }
+}
--- api-ecommerce/routes/api.php
+++ api-ecommerce/routes/api.php
@@ -3,16 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Api\AdminController;
 
 Route::post("register", [ApiController::class, "register"]);
 Route::post("login", [ApiController::class, "login"]);
 Route::post("admin_login", [ApiController::class, "adminlogin"]);
 
-Route::group([
-    "middleware" => ["auth:api"]
-], function(){
+Route::group(["middleware" => ["auth:api"]], function(){
 
     Route::get("profile", [ApiController::class, "profile"]);
     Route::get("refresh", [ApiController::class, "refreshToken"]);
     Route::get("logout", [ApiController::class, "logout"]);
+});
+
+Route::group(["middleware" => ["auth:api"]], function(){
+
+    Route::get("admin/users/latest", [AdminController::class, "getLastUsers"]);
+  
 });
