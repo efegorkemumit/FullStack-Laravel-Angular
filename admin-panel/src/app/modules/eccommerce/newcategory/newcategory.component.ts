d7bf0e58d8fea951029be17525ca9323973be1ab
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-10-31 20:18:34 +0300

    image prewiew
    Youtube Efe Görkem Ümit

--- admin-panel/src/app/modules/eccommerce/newcategory/newcategory.component.ts
+++ admin-panel/src/app/modules/eccommerce/newcategory/newcategory.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EccomerceService } from '../_services/eccomerce.service';
 
 @Component({
   selector: 'app-newcategory',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./newcategory.component.css']
 })
 export class NewcategoryComponent {
+
+  name:any=null;
+  icon:any=null;
+  images_file:any=null;
+
+  images_preview:any =null;
+
+  registrationSuccess = false;
+  successMessage= "Registtration Successful"
+
+
+  constructor(
+    public eccommerceService: EccomerceService
+  ){}
+
+  processFile(event : Event){
+    const target = event.target as HTMLInputElement;
+    if (target.files && target.files[0]) {
+      if (target.files[0].type.indexOf("image") < 0) {
+        console.log("Resim dosyası değil");
+        return;
+      }
+      this.images_file = target.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(this.images_file);
+      reader.onloadend = () => (this.images_preview = reader.result);
+    }
+    
+  }
+
 
 }
