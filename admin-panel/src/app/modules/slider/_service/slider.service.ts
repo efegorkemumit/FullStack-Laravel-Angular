630001fbfa032b48b295c18b92a818ae729630ec
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-02 14:48:58 +0300

    Service

--- admin-panel/src/app/modules/slider/_service/slider.service.ts
+++ admin-panel/src/app/modules/slider/_service/slider.service.ts
@@ -1,9 +1,91 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { URL_SERVICE } from 'src/config/config';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SliderService {
 
-  constructor() { }
+ 
+  constructor(private http: HttpClient) { }
+
+  getCategory():Observable<any>{
+    let URL = URL_SERVICE + '/slider/all';
+
+   
+
+    return this.http.get<any>(URL,);
+      
+  }
+
+  deletecategory(userId:number):Observable<any>{
+    let URL = URL_SERVICE + '/slider/' +userId;
+
+    const token = localStorage.getItem('token');
+
+    if(!token)
+    {
+      return of(null);
+    }
+    const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`,}
+    );
+
+    return this.http.delete<any>(URL, {headers});
+      
+  }
+
+  create(data:any):Observable<any>{
+    let URL = URL_SERVICE + '/slider/add';
+
+    const token = localStorage.getItem('token');
+
+    if(!token)
+    {
+      return of(null);
+    }
+    const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`,}
+    );
+
+    return this.http.post<any>(URL, data, {headers});
+      
+  }
+  getSliderDetail(id:number):Observable<any>{
+    let URL = URL_SERVICE + '/slider/detail/' +id;
+
+    const token = localStorage.getItem('token');
+
+    if(!token)
+    {
+      return of(null);
+    }
+    const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`,}
+    );
+
+    return this.http.get<any>(URL, {headers});
+      
+  }
+
+  
+  update(id:number, data:any):Observable<any>{
+    let URL = URL_SERVICE + '/slider/update/' +id;
+
+    const token = localStorage.getItem('token');
+
+    if(!token)
+    {
+      return of(null);
+    }
+    const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`,}
+    );
+
+    return this.http.post<any>(URL, data,  {headers});
+      
+  }
 }
