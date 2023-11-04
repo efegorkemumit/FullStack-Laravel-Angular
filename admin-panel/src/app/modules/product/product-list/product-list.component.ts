0672bd9da5d9da3376de287483c38086c52a0a84
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-05 00:28:05 +0300

    Product List
    Youtube Efe Görkem Ümit

--- admin-panel/src/app/modules/product/product-list/product-list.component.ts
+++ admin-panel/src/app/modules/product/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EccomerceService } from '../../eccommerce/_services/eccomerce.service';
+import { URL_BACKEND } from 'src/config/config';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+products:any[] =[];
+  orginalproducts:any[] =[];
+  searchText:string = '';
+  userId:number|undefined;
+  URL=URL_BACKEND
+  
 
+  constructor(
+    public EccommerceService:EccomerceService
+  ){}
+
+  ngOnInit(){
+    this.EccommerceService.getCategory().subscribe((data:any)=>{
+      this.products = data['categories'];
+      this.orginalproducts= data['categories'];
+    })
+  }
+  onSearch(){
+    if(this.searchText===''){
+      this.products = this.orginalproducts;
+    }
+    else{
+      this.products=this.orginalproducts.filter(category=>{
+        return category.name.toLowerCase().includes(this.searchText.toLowerCase()) 
+            
+      })
+    }
+  }
+  deleteCategory(id:number){
+    this.EccommerceService.deletecategory(id).subscribe(response=>{
+
+      this.EccommerceService.getCategory().subscribe((data:any)=>{
+        this.products = data['products'];
+        this.orginalproducts= data['products'];
+      });
+
+    },error=>{
+      console.error("User Delete Failed", error);
+    })
+  }
 }
