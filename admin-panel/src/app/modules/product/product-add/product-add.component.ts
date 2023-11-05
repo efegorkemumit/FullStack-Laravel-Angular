63a92578ac7bdbd2f88433c025a794fada36abc4
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-06 00:58:48 +0300

    Add Tags
    Youtube Efe Görkem Ümit

--- admin-panel/src/app/modules/product/product-add/product-add.component.ts
+++ admin-panel/src/app/modules/product/product-add/product-add.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent {
+  tagsName:any=null;
+  tags:any=[];
+
+  addTags(){
+
+    if(this.tagsName!=null){
+      this.tags.push(this.tagsName)
+      this.tagsName=null;
+    }
+  
+  }
+
+  removeTags(index:number)
+  {
+    this.tags.splice(index,1);
+
+  }
 
 }
