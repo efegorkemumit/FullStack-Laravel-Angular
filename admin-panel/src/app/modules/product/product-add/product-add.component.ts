e36c7f8fe0827d82d08ef196c5443426263c9fce
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-06 09:58:35 +0300

    images

--- admin-panel/src/app/modules/product/product-add/product-add.component.ts
+++ admin-panel/src/app/modules/product/product-add/product-add.component.ts
@@ -9,6 +9,46 @@ export class ProductAddComponent {
   tagsName:any=null;
   tags:any=[];
 
+  images_files:any=[];
+  img_files:any=null;
+  img_previews:any=null;
+
+  addFiles(event:Event)
+  {
+    const target  = event.target as HTMLInputElement;
+    if(target.files && target.files[0]){
+      if(target.files[0].type.indexOf("image")<0){
+        return;
+      }
+      this.img_files =target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(this.img_files);
+      reader.onloadend =()=> (this.img_previews = reader.result);
+    }
+
+  }
+  addImages(){
+
+    if(this.img_files !=null){
+      this.images_files.push({
+        file:this.img_files,
+        show:this.img_previews
+      });
+      this.img_files =null;
+      this.img_previews =null
+
+    }
+
+   
+
+  }
+
+  removeImages(index:number)
+  {
+    this.images_files.splice(index,1);
+
+  }
+
   addTags(){
 
     if(this.tagsName!=null){
