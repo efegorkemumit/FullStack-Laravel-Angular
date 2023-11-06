6c145645232359ce0ed0675a12468fd6c1e52744
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-06 11:42:34 +0300

    cover images
    Youtube Efe Görkem Ümit

--- admin-panel/src/app/modules/product/product-add/product-add.component.ts
+++ admin-panel/src/app/modules/product/product-add/product-add.component.ts
@@ -13,6 +13,26 @@ export class ProductAddComponent {
   img_files:any=null;
   img_previews:any=null;
 
+
+  images_file:any=null;
+
+  images_preview:any =null;
+
+  processFile(event : Event){
+    const target = event.target as HTMLInputElement;
+    if (target.files && target.files[0]) {
+      if (target.files[0].type.indexOf("image") < 0) {
+        console.log("Resim dosyası değil");
+        return;
+      }
+      this.images_file = target.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(this.images_file);
+      reader.onloadend = () => (this.images_preview = reader.result);
+    }
+    
+  }
+
   addFiles(event:Event)
   {
     const target  = event.target as HTMLInputElement;
