c12eb170762ede80de95748e0b392d44502a00c5
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-07 23:52:09 +0300

    Resource
    Youtube Efe görkem Ümit

--- api-ecommerce/app/Http/Resources/ProductResource.php
+++ api-ecommerce/app/Http/Resources/ProductResource.php
@@ -8,12 +8,47 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class ProductResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
+    
      *
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return[
+            "id"=>$this->id,
+            "title"=>$this->resource->title,
+            "category_id"=>$this->resource->category_id,
+            "category"=>[
+                "id"=>$this->resource->category->id,
+                "name"=>$this->resource->category->name,
+                "icon"=>$this->resource->category->icon,
+                "images"=>$this->resource->category->images,
+            ],
+            "slug"=>$this->resource->slug,
+            "sku"=>$this->resource->sku,
+            "price_dsc"=>$this->resource->price_dsc,
+            "price_usd"=>$this->resource->price_usd,
+            "tags"=>$this->resource->tags,
+            "description"=>$this->resource->description,
+            "summary"=>$this->resource->summary,
+            "image"=>$this->resource->images,
+            "images"=>$this->resource->images->map(function($img){
+                return[
+                    "id"=>$img->id,
+                    "file_name"=>$img->file_name,
+                    "size"=>$img->size,
+                    "type"=>$img->type,
+                    "images"=>env("APP_URL")."storage/".$img->images,
+                ];
+            }),
+           "sizes"=>$this->resource->size->map(function($size){
+                return[
+                    "id"=>$size->id,
+                    "name"=>$size->name,
+
+                ];
+           }),
+
+        ];
     }
 }
