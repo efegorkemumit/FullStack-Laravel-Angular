65e0d41fc8f7e699f0061312bb39d16b993fa863
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-12 20:22:06 +0300

    Cupon servis
    Youtube Efe Görkem Ümit

--- admin-panel/src/app/modules/cupones/_services/cupon.service.ts
+++ admin-panel/src/app/modules/cupones/_services/cupon.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,69 @@ import { Injectable } from '@angular/core';
 })
 export class CuponService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCupon():Observable<any>{
+    let URL = URL_SERVICE + '/cupons/all';
+
+   
+
+    return this.http.get<any>(URL,);
+      
+  }
+
+  configall():Observable<any>{
+    let URL = URL_SERVICE + '/cupons/config-all';
+
+   
+
+    return this.http.get<any>(URL,);
+      
+  }
+
+
+  getShowCupon(id:number):Observable<any>{
+    let URL = URL_SERVICE + '/cupons/show/' +id;
+
+    
+
+    return this.http.get<any>(URL, {headers});
+      
+  }
+
+  create(data:any):Observable<any>{
+    let URL = URL_SERVICE + '/cupons/add';
+
+    const token = localStorage.getItem('token');
+
+    if(!token)
+    {
+      return of(null);
+    }
+    const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`,}
+    );
+
+    return this.http.post<any>(URL, data, {headers});
+      
+  }
+
+  deletecategory(id:number):Observable<any>{
+    let URL = URL_SERVICE + '/cupons/delete/' +id;
+
+    const token = localStorage.getItem('token');
+
+    if(!token)
+    {
+      return of(null);
+    }
+    const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`,}
+    );
+
+    return this.http.delete<any>(URL, {headers});
+      
+  }
+
+
 }
