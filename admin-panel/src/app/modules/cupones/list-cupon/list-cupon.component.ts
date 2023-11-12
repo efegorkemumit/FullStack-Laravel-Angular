9f4e0b5f6e4520d324340ca110526401fe7252b5
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-12 13:34:10 +0300

    cupon list

--- admin-panel/src/app/modules/cupones/list-cupon/list-cupon.component.ts
+++ admin-panel/src/app/modules/cupones/list-cupon/list-cupon.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { URL_BACKEND } from 'src/config/config';
+import { EccomerceService } from '../../eccommerce/_services/eccomerce.service';
 
 @Component({
   selector: 'app-list-cupon',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-cupon.component.css']
 })
 export class ListCuponComponent {
+  categories:any[] =[];
+  orginalcategories:any[] =[];
+  searchText:string = '';
+  userId:number|undefined;
+  URL=URL_BACKEND
+  
+
+  constructor(
+    public EccommerceService:EccomerceService
+  ){}
+
+  ngOnInit(){
+    this.EccommerceService.getCategory().subscribe((data:any)=>{
+      this.categories = data['categories'];
+      this.orginalcategories= data['categories'];
+    })
+  }
+  onSearch(){
+    if(this.searchText===''){
+      this.categories = this.orginalcategories;
+    }
+    else{
+      this.categories=this.orginalcategories.filter(category=>{
+        return category.name.toLowerCase().includes(this.searchText.toLowerCase()) 
+            
+      })
+    }
+  }
+  deleteCategory(id:number){
+  
+  }
+  
 
 }
