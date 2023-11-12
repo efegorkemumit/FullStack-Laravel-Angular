7acc0fe530043cb3c1235ea73357f6c5fe3b9185
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-12 16:44:13 +0300

    Add Cupon Desing 2

--- admin-panel/src/app/modules/cupones/add-new-cupon/add-new-cupon.component.ts
+++ admin-panel/src/app/modules/cupones/add-new-cupon/add-new-cupon.component.ts
@@ -7,13 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AddNewCuponComponent {
 
+  code:any;
   type_cupon:any=1;
+  type_discount:any =1;
+  discount:any=null;
+  type_count:any=1;
+  num_use:any=0;
 
+  product_id:any =null;
+  category_id:any =null;
+
+  categories:any = [];
+  products:any=[];
 
  checkedType(value:any)
  {
   this.type_cupon =value;
 
+ }
+
+ checkTypeD(value:any){
+  this.type_discount =value;
+
+ }
+
+ checkTypeC(value:any){
+  this.type_count=value;
  }
 
 }
