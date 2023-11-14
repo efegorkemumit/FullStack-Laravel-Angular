781fd5deb38b84e6cac5a942df263fdd05d26a57
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-14 13:40:44 +0300

    Discount Service

--- admin-panel/src/app/modules/discount/_services/discount.service.ts
+++ admin-panel/src/app/modules/discount/_services/discount.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,75 @@ import { Injectable } from '@angular/core';
 })
 export class DiscountService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getDiscount():Observable<any>{
+    let URL = URL_SERVICE + '/discount/all';
+
+   
+
+    return this.http.get<any>(URL,);
+      
+  }
+
+  getShowDiscount(id:number):Observable<any>{
+    let URL = URL_SERVICE + '/discount/show_discount/' +id;
+
+    
+
+    return this.http.get<any>(URL);
+      
+  }
+
+  update(id:number, data:any):Observable<any>{
+    let URL = URL_SERVICE + '/discount/update/' +id;
+
+    const token = localStorage.getItem('token');
+
+    if(!token)
+    {
+      return of(null);
+    }
+    const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`,}
+    );
+
+    return this.http.post<any>(URL, data,  {headers});
+      
+  }
+
+  create(data:any):Observable<any>{
+    let URL = URL_SERVICE + '/discount/add';
+
+    const token = localStorage.getItem('token');
+
+    if(!token)
+    {
+      return of(null);
+    }
+    const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`,}
+    );
+
+    return this.http.post<any>(URL, data, {headers});
+      
+  }
+
+  deletediscount(id:number):Observable<any>{
+    let URL = URL_SERVICE + '/discount/delete/' +id;
+
+    const token = localStorage.getItem('token');
+
+    if(!token)
+    {
+      return of(null);
+    }
+    const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`,}
+    );
+
+    return this.http.delete<any>(URL, {headers});
+      
+  }
+
+
 }
