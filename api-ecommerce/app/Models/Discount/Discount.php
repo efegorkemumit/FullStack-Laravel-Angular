7cbe0538784e6caa444b46edafd1a1577131daa9
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-14 09:44:30 +0300

    scopeValidateDiscount

--- api-ecommerce/app/Models/Discount/Discount.php
+++ api-ecommerce/app/Models/Discount/Discount.php
@@ -28,4 +28,25 @@ class Discount extends Model
     {
         return $this->hasMany(DiscountCategorie::class);
     }
+
+    public function scopeValidateDiscount($query, $request, $product_array=[], $categorie_array=[])
+    {
+        $query->where("type", $request->type);
+        if($request->type==1)
+        {
+            $query->whereHas("products",function($q) use($product_array){
+                return $q->whereIn("product_id", $product_array);
+
+            });
+        }
+        if($request->type==2)
+        {
+            $query->whereHas("categories",function($q) use($categorie_array){
+                return $q->whereIn("categorie_id", $categorie_array);
+
+            });
+        }
+        
+        return $query;
+    }
 }
