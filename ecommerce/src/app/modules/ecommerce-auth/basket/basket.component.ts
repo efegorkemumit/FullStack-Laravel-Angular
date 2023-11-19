8a1ceb4b2c7cfd2a9348f9858abb12347a243f67
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-19 14:29:50 +0300

    basket 1

--- ecommerce/src/app/modules/ecommerce-auth/basket/basket.component.ts
+++ ecommerce/src/app/modules/ecommerce-auth/basket/basket.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CartServicesService } from '../_services/cart-services.service';
+import { AuthService } from '../../auth-profile/_services/auth.service';
 
 @Component({
   selector: 'app-basket',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./basket.component.css']
 })
 export class BasketComponent {
+
+  listCarts:any = [];
+
+
+  constructor(
+
+    public cartService:CartServicesService,
+    public auth:AuthService,
+  ){}
+
+  ngOnInit():void{
+
+    if(!this.auth.user){
+      console.log("user not authenticated");
+      return;
+    }
+  }
 
 }
