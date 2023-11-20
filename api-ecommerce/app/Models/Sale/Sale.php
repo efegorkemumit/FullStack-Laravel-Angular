0694a57424d5544a190fe4cb297fa2e943c756e0
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-21 01:54:18 +0300

    Model Ayarlamaları

--- api-ecommerce/app/Models/Sale/Sale.php
+++ api-ecommerce/app/Models/Sale/Sale.php
@@ -4,8 +4,37 @@ namespace App\Models\Sale;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\User;
 
 class Sale extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
+    protected $fillable = [
+        'user_id', 
+        'total', 
+        'subtotal', 
+       
+     ];
+
+     public function user()
+     {
+        return $this->belongsTo(User::class);
+
+     }
+     public function sale_address()
+     {
+        return $this->hasOne(SaleAddress::class);
+
+     }
+     public function sale_details()
+     {
+        return $this->hasMany(SaleDetail::class);
+
+     }
+     
+
+
+
+
 }
