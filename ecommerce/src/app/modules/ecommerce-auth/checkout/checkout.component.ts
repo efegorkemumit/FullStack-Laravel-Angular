400f58b5253205f2c109e32ff6e8d6b4e1dcf363
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-11-20 13:58:36 +0300

    Checkout Review

--- ecommerce/src/app/modules/ecommerce-auth/checkout/checkout.component.ts
+++ ecommerce/src/app/modules/ecommerce-auth/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CartServicesService } from '../_services/cart-services.service';
+import { AuthService } from '../../auth-profile/_services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
@@ -15,6 +18,55 @@ export class CheckoutComponent {
   zip_code:any = null;
   phone:any = null;
   email:any = null;
+  listCarts:any = [];
+
+  Subtotal:any = 0;
+  TotalPrice:any=0;
  
+
+  constructor(
+
+    public cartService:CartServicesService,
+    public auth:AuthService,
+    public router:Router
+  ){}
+
+
+  ngOnInit():void{
+
+    if(!this.auth.user){
+      this.router.navigate(['/login'])
+
+    }
+
+    this.cartService.basketlist().subscribe((resp:any)=>{
+      console.log(resp);
+
+      if(resp && resp.carts && resp.carts.data)
+      {
+        this.listCarts=resp.carts.data;
+        this.calculateTotalPrice();
+      }
+      else
+      {
+        console.log("Invalid", resp);
+      }
+
+    })
+  }
+
+  calculeteTotal():number{
+    return this.listCarts.reduce((sum:number, item:any)=> sum +item.total,0 )
+  }
+
+  calculeteSubTotal():number{
+    return this.listCarts.reduce((sum:number, item:any)=> sum +item.subtotal,0 )
+  }
+
+  calculateTotalPrice():void
+  {
+    this.Subtotal = this.calculeteSubTotal();
+    this.TotalPrice = this.calculeteTotal();
+  }
 
 }
